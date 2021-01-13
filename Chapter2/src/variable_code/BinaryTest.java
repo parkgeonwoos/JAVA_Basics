7b41c2d840c31dca62e0fb5c3c9c984f774023b1
parkgeonwoo <50697545+parkgeonwoos@users.noreply.github.com>
2021-01-13 20:50:47 +0900

    Update BinaryTest.java
    "java-programming"

--- Chapter2/src/variable_code/BinaryTest.java
+++ Chapter2/src/variable_code/BinaryTest.java
@@ -3,9 +3,9 @@ package variable_code;
 public class BinaryTest {
 	public static void main(String[]args) {
 		int num = 10;//10진수 선언
-		int bNum = 0B1010;//2진수 선언(10)
-		int oNum = 012;//8진수 선언(10)
-		int xNum = 0XA;//16진수 선언(10)
+		int bNum = 0B1010;//2진수 선언(값:10)
+		int oNum = 012;//8진수 선언(값:10)
+		int xNum = 0XA;//16진수 선언(값:10)
 		System.out.println(num);
 		System.out.println(bNum);
 		System.out.println(oNum);
